a489dad738efff7b50de1f91d0da8351dfbd20ff
psanvicente21 <psanvicente1986@gmail.com>
2023-06-19 10:31:54 +0200

    Fixed URL to result view

--- polls/views.py
+++ polls/views.py
@@ -20,7 +20,7 @@ def detail(request, question_id):
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+    return render(request, "results.html", {"question": question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
