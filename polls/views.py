0b804c3f5b84db727f2633e42b2ea784eb995c57
psanvicente21 <psanvicente1986@gmail.com>
2023-06-19 10:50:31 +0200

    deleted old views: index, detail, result. Added clases: IndexView, Detail,View. ResultsView

--- polls/views.py
+++ polls/views.py
@@ -2,25 +2,30 @@ from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 from .models import Question, Choice
 
 # Create your views here.
 
 from django.http import HttpResponse
 
+class IndexView(generic.ListView):
+    template_name = "index.html"
+    context_object_name = "latest_question_list"
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "index.html", context)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "detail.html", {"question": question})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "results.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
